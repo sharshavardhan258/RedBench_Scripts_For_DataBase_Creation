5dc58efa170f432faa23e5a87a144774354e712e
sharshavardhan258 <sharshavardhan258@gmail.com>
2017-07-06 17:39:45 +0530

    Create Technical Skills.sql

---
+++ Technical Skills.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[Technical Skills] (
+    [Id]                   INT           IDENTITY (106000, 1) NOT NULL,
+    [Technical Skill Name] VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
